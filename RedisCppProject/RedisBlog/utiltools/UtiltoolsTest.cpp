109057c8769314491630b9164dd57cd0d513a5cd
andrew2_liu <andrew2_liu@askey.com>
2021-11-23 10:08:42 +0800

    add compile attribute

--- RedisCppProject/RedisBlog/utiltools/UtiltoolsTest.cpp
+++ RedisCppProject/RedisBlog/utiltools/UtiltoolsTest.cpp
@@ -9,17 +9,38 @@ struct RedisUtiltoolsTest : public testing::Test
 {
     void SetUp()override
     {
-
+        ctxt = redisConnect("127.0.0.1", 6379);
     }
 
     void TearDown()override
     {
+        if (reply)
+        {
+           freeReplyObject(reply);
+           reply = nullptr;
+        }
+        
+        if (ctxt)
+        {
+            redisFree(ctxt);
+            ctxt = nullptr;
+        }
+        
+    }
+    RedisUtiltoolsTest():ctxt(nullptr), 
+                         reply(nullptr),
+                         rediscommand("")
+    {
 
     }
+protected:
+    redisContext   *ctxt;
+    redisReply     *reply;
+    std::string     rediscommand;
 };
 
 
 TEST_F(RedisUtiltoolsTest, redis_global_repo_is_safety)
 {
-
+    EXPECT_NE(ctxt, nullptr);
 }
