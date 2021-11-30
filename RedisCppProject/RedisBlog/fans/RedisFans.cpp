4c0f8d7ef4cdf6ddb5cf2491d839562511595c69
andrew2_liu <andrew2_liu@askey.com>
2021-11-30 20:02:53 +0800

    add fans function

--- RedisCppProject/RedisBlog/fans/RedisFans.cpp
+++ RedisCppProject/RedisBlog/fans/RedisFans.cpp
@@ -1,7 +1,9 @@
 #include "RedisFans.h"
 
 
-RedisFans::RedisFans()
+RedisFans::RedisFans(RedisUtil& util) : re_command(""),
+                         re_key(""),
+                         util(util)
 {
 
 }
@@ -9,4 +11,112 @@ RedisFans::RedisFans()
 RedisFans::~RedisFans()
 {
     
+}
+
+RedisResult RedisFans::Follow(std::string& fans, std::string& target_id)
+{
+    //add fans to target's fans list
+    MakeFansKey(target_id);
+    MakeFollowCmd(fans);
+    if ( RedisResult::OK != util.ExecCommand(re_command))
+    {
+        return RedisResult::ERROR;
+    }
+    util.FreeReply();
+
+    //add target to fans's following list
+    MakeFollowKey(fans);
+    MakeFansCms(target_id);
+    if (RedisResult::OK != util.ExecCommand(re_command))
+    {
+        return RedisResult::ERROR;
+    }
+    util.FreeReply();
+    
+    return RedisResult::OK;
+}
+
+bool RedisFans::IsFollow(std::string& fans, std::string& target_id)
+{
+    MakeFollowKey(fans);
+    MakeFollowCmd(target_id);
+    if (RedisResult::OK != util.ExecCommand(re_command))
+    {
+        return false;
+    }
+    return true;
+}
+
+bool RedisFans::IsFollowEachOther(std::string& fans, std::string& target_id)
+{
+    bool result = IsFollow(fans, target_id) && IsFollow(target_id, fans);
+    return result;
+}
+
+std::vector<std::string>& RedisFans::GetAllFollows(std::string& user_id)
+{
+    MakeFollowKey(user_id);
+    MakeFollowCmd(user_id);
+    if (RedisResult::OK != util.ExecCommand(re_command))
+    {
+        return vec;
+    }
+    return vec;
+}
+
+std::vector<std::string>& RedisFans::GetAllFans(std::string& user_id)
+{
+    MakeFollowKey(user_id);
+    MakeFollowCmd(user_id);
+    if (RedisResult::OK != util.ExecCommand(re_command))
+    {
+        return vec;
+    }
+    return vec;
+}
+
+std::vector<std::string>& RedisFans::GetCommonFollows(std::string& user_id, std::string& target_id)
+{
+    MakeFollowKey(user_id);
+    MakeFollowKey(target_id);
+    if (RedisResult::OK != util.ExecCommand(re_command))
+    {
+        return vec;
+    }
+    return vec;
+}
+
+void RedisFans::MakeFollowKey(std::string& user_id)
+{
+    re_key.assign("weibo::user::");
+    re_key += user_id;
+    re_key += "::following";
+}
+
+void RedisFans::MakeFansKey(std::string& fans_id)
+{
+    re_key.assign("weibo::user::");
+    re_key += fans_id;
+    re_key += "::fans";
+}
+
+void RedisFans::MakeFollowCmd(std::string& fans_id)
+{
+    re_command.assign("SADD ");
+    re_command += re_key;
+    re_command += " ";
+    re_command += fans_id;
+}
+
+void RedisFans::MakeFansCms(std::string& target_id)
+{
+    re_command.assign("SADD ");
+    re_command += re_key;
+    re_command += " ";
+    re_command += target_id;
+}
+
+void RedisFans::Clear()
+{
+    vec.clear();
 }
--- RedisCppProject/RedisBlog/fans/RedisFans.h
+++ RedisCppProject/RedisBlog/fans/RedisFans.h
@@ -1,7 +1,31 @@
 #pragma once
 
+#include <vector>
+#include "RedisUtil.h"
+
 struct RedisFans
 {
-    RedisFans();
+    RedisFans(RedisUtil& util);
     ~RedisFans();
+
+    RedisResult Follow(std::string& fans, std::string& target_id);
+    bool IsFollow(std::string& fans, std::string& target_id);
+    bool IsFollowEachOther(std::string& fans, std::string& target_id);
+    std::vector<std::string>& GetAllFollows(std::string& user_id);
+    std::vector<std::string>& GetAllFans(std::string& user_id);
+    std::vector<std::string>& GetCommonFollows(std::string& user_id, std::string& target_id);
+
+    void Clear();
+
+private:
+    void MakeFollowKey(std::string& user_id);
+    void MakeFansKey(std::string& fans_id);
+    void MakeFollowCmd(std::string& fans_id);
+    void MakeFansCms(std::string& target_id);
+
+private:
+    std::string re_command;
+    std::string re_key;
+    RedisUtil& util;
+    std::vector<std::string> vec;
 };
